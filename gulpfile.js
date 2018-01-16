1a4a0f480b9d3edbb5b8199d3330e4683ef39a19
thomasbck <thomasbrunck@gmail.com>
2018-01-16 15:10:48 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+ 
+gulp.task('styles', function() {
+ 
+    gulp.src('sass/**/*.scss') //source à inspecter
+    	.pipe(sourcemaps.init()) //initialisation de sourcemap
+        .pipe(sass().on('error', sass.logError)) //affichage des erreurs si il y a
+        .pipe(sourcemaps.write()) //correspondances de fichier sass>css  
+        .pipe(autoprefixer()) //prefixer les propriétés css au besoin
+        .pipe(gulp.dest('./')) //destination des fichiers compilés sass en css
+ 
+});
+ 
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
