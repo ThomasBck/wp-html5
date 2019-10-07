a3f11889374520c0b9ff46e35257a1c0a511fc18
Thomas <thomasbrunck@gmail.com>
2019-10-07 10:36:50 +0200

    sanitize-file-upload and add xml sitemap automatically

--- functions.php
+++ functions.php
@@ -8,6 +8,34 @@
 /*------------------------------------*\
 	External Modules/Files
 \*------------------------------------*/
+require_once'sanitize-file-upload.php';
+
+function xml_sitemap() {
+    $postsForSitemap = get_posts(array(
+      'numberposts' => -1,
+      'orderby' => 'modified',
+      'post_type'  => array('post','page'),
+      'order'    => 'DESC'
+    ));
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+    $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    foreach($postsForSitemap as $post) {
+      setup_postdata($post);
+      $postdate = explode(" ", $post->post_modified);
+      $sitemap .= '<url>'.
+        '<loc>'. get_permalink($post->ID) .'</loc>'.
+        '<lastmod>'. $postdate[0] .'</lastmod>'.
+        '<changefreq>monthly</changefreq>'.
+      '</url>';
+    }
+    $sitemap .= '</urlset>';
+    $fp = fopen(ABSPATH . "sitemap.xml", 'w');
+    fwrite($fp, $sitemap);
+    fclose($fp);
+  }
+  add_action("publish_post", "xml_sitemap");
+  add_action("publish_page", "xml_sitemap");
+  
 
 // Load any external files you have here
 
