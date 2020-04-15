b1765cd51247a22f4cf8e8da03f3500a0da9d0a7
Thomas <thomasbrunck@gmail.com>
2020-04-15 14:54:16 +0200

    update Bootstrap to 4.4

--- functions.php
+++ functions.php
@@ -8,7 +8,9 @@
 /*------------------------------------*\
 	External Modules/Files
 \*------------------------------------*/
-require_once'sanitize-file-upload.php';
+require_once'assets/sanitize-file-upload.php';
+require_once'assets/class-wp-bootstrap-navwalker.php';
+require_once'assets/admin-custom-fields.php';
 
 function xml_sitemap() {
     $postsForSitemap = get_posts(array(
@@ -44,14 +46,10 @@ function xml_sitemap() {
 \*------------------------------------*/
 function mode_maintenance() {
     if( !is_user_logged_in() || !current_user_can('administrator')) {
-    wp_die('<div style="padding:5rem;"><div style="text-align:center;"><img src="'.get_stylesheet_directory_uri().'/img/logo.svg" style="width:50%;"/></div><p style="color:#444; text-align:center;">Nous effectuons des mises à jour sur notre site internet afin de rendre meilleur son expérience, Level2 revient rapidement.</p>');
+    wp_die('<div style="padding:5rem;"><div style="text-align:center;"><img src="'.get_stylesheet_directory_uri().'/img/logo.svg" style="width:50%;"/></div><p style="color:#444; text-align:center;">Nous effectuons des mises à jour sur notre site internet afin de rendre meilleur son expérience, '.get_bloginfo( 'name' ).' revient rapidement.</p>');
     }
 }
-//add_action('get_header', 'mode_maintenance');// activer si on veut le mode maintenance
 
-
-/* Add navwalker class to implement bootstrap nav */ 
-require_once('class-wp-bootstrap-navwalker.php');
 
 /* change link on logo (login page)*/
 function my_login_logo_url() {
@@ -75,105 +73,6 @@ function my_login_logo() { ?>
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
-
-
-
-//add admin theme panel 
-function l2_add_admin_page(){
-    //First level
-    add_menu_page('Theme options', 'Level2', 'manage_options', 'page_l2', 'l2_theme_create_page', get_template_directory_uri().'/img/icons/admin-icon.png', 61 );
-
-    //second level create submenu page with same 
-    //add_submenu_page('page_l2', 'Nerya Theme options', 'settings', 'manage_options', 'page_l2', 'nerya_theme_create_page' );
-    //add_submenu_page('page_l2', 'Nerya css option', 'custom css', 'manage_options', 'page_l2_css', 'nerya_theme_css_page' );
-
-}
- 
-add_action('admin_menu', 'l2_add_admin_page');
-add_action('admin_init', 'l2_settings');
-
-function l2_settings(){
-    register_setting('l2-settings-group','l2_adresse');
-    register_setting('l2-settings-group','l2_tel');
-    register_setting('l2-settings-group','l2_mail');
-    register_setting('l2-settings-group','l2_linkedin');
-    register_setting('l2-settings-group','l2_vimeo');
-
-    add_settings_section('l2-sidebar-options','Options de base', 'l2_sidebar_options' ,'page_l2');
-
-    add_settings_section('l2-sidebar-social','Réseaux sociaux', 'l2_sidebar_social' ,'page_l2');
-
-
-
-    add_settings_field('sidebar-adresse','Adresse','l2_sidebar_adresse','page_l2','l2-sidebar-options');
-
-
-    add_settings_field('sidebar-tel','Tel','l2_sidebar_tel','page_l2','l2-sidebar-options');
-    add_settings_field('sidebar-mail','E-mail','l2_sidebar_mail','page_l2','l2-sidebar-options');
-
-
-     add_settings_field('sidebar-linkedin','Linkedin','l2_sidebar_social_linkedin','page_l2','l2-sidebar-social');
-    add_settings_field( 'sidebar-vimeo', 'Vimeo', 'l2_sidebar_social_vimeo', 'page_l2', 'l2-sidebar-social' );
-
-}
-
-function l2_sidebar_options(){
-    echo 'Information de base';
-}
-function l2_sidebar_social(){
-    echo 'Liens vers les réseaux sociaux';
-}
-
-
-function l2_sidebar_adresse(){
-    $adresse = esc_attr(get_option( 'l2_adresse' ));
-    echo '<input type="text" name="l2_adresse" value="'.$adresse.'" class="regular-text" placeholder="Adresse de société" />';
-}
-function l2_sidebar_tel(){
-    $tel = esc_attr(get_option( 'l2_tel' ));
-    echo '<input type="text" name="l2_tel" value="'.$tel.'" class="regular-text" placeholder="Telephone" />';
-}
-
-function l2_sidebar_mail(){
-    $mail = esc_attr(get_option( 'l2_mail' ));
-    echo '<input type="text" name="l2_mail" value="'.$mail.'" class="regular-text" placeholder="Email" />';
-}
-
-function l2_sidebar_social_linkedin(){
-    $linkedin = esc_attr(get_option( 'l2_linkedin' ));
-    echo '<input type="text" name="l2_linkedin" value="'.$linkedin.'" class="regular-text" placeholder="Lien profil LinkedIn" /> ';
-}
-
-function l2_sidebar_social_vimeo(){
-    $vimeo = esc_attr(get_option( 'l2_vimeo' ));
-    echo '<input type="text" name="l2_vimeo" value="'.$vimeo.'" class="regular-text" placeholder="Lien profil Vimeo" />';
-}
-
-
- function l2_theme_create_page(){
-    //generation of admin page
-
-    require_once(get_template_directory().'/admin/admin.php');
-
- }
-
-// [info type="tel"]
-function bartag_func( $atts ) {
-    $a = shortcode_atts( array(
-        'type' => 'tel'
-        ), $atts );
-    $value = 'l2_'.$atts['type'];
-
-    return esc_attr(get_option( 'l2_'.$a['type'] ));
-}
-add_shortcode( 'info', 'bartag_func' );
-
-
-
- //end admin theme panel
-
-
-
 if (function_exists('add_theme_support'))
 {
     // Add Menu Support
@@ -185,25 +84,6 @@ if (function_exists('add_theme_support'))
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
-
-    // Add Support for Custom Backgrounds - Uncomment below if you're going to use
-    /*add_theme_support('custom-background', array(
-	'default-color' => 'FFF',
-	'default-image' => get_template_directory_uri() . '/img/bg.jpg'
-    ));*/
-
-    // Add Support for Custom Header - Uncomment below if you're going to use
-    /*add_theme_support('custom-header', array(
-	'default-image'			=> get_template_directory_uri() . '/img/headers/default.jpg',
-	'header-text'			=> false,
-	'default-text-color'		=> '000',
-	'width'				=> 1000,
-	'height'			=> 198,
-	'random-default'		=> false,
-	'wp-head-callback'		=> $wphead_cb,
-	'admin-head-callback'		=> $adminhead_cb,
-	'admin-preview-callback'	=> $adminpreview_cb
-    ));*/
 
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
@@ -239,16 +119,16 @@ function html5blank_header_scripts()
 {
     if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
 
-        wp_register_script('myjquery', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), '1.0.0'); // Custom scripts
+        wp_register_script('myjquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '1.0.0'); // Custom scripts
         wp_enqueue_script('myjquery'); // Enqueue it! 
 
-        wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array(), '1.0.0'); // Custom scripts
+        wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.0.0'); // Custom scripts
         wp_enqueue_script('popper'); // Enqueue it! 
 
         // wp_register_script('backstretch', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js', array(), '1.0.0'); // Custom scripts
         // wp_enqueue_script('backstretch'); // Enqueue it!  
 
-        wp_register_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array(), '1.0.0'); // Custom scripts
+        wp_register_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '1.0.0'); // Custom scripts
         wp_enqueue_script('bootstrap-js'); // Enqueue it!    	
 
         wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
